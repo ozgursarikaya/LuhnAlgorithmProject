7aaa8554209bce4ad26e046ae36651ca9121fb45
DSANS\osarikaya <ozgur.sarikaya@nesine.com>
2024-10-22 10:58:17 +0300

    Add project files.

---
+++ LuhnAlgorithmProject/LuhnAlgorithm.cs
@@ -0,0 +1,38 @@
+﻿namespace LuhnAlgorithmProject
+{
+    internal class LuhnAlgorithm
+    {
+        internal static bool IsValid(string cardNumber)
+        {
+            int sum = 0;
+            bool isSecond = false;
+
+            // Kart numarasını sağdan sola doğru işle
+            for (int i = cardNumber.Length - 1; i >= 0; i--)
+            {
+                int digit = cardNumber[i] - '0';
+
+                // Her ikinci rakamı ikiyle çarp
+                if (isSecond)
+                {
+                    digit *= 2;
+
+                    // Eğer çarpım 9'dan büyükse, rakamları topla (örneğin 12 için: 1 + 2 = 3)
+                    if (digit > 9)
+                    {
+                        digit -= 9;
+                    }
+                }
+
+                // Toplamı güncelle
+                sum += digit;
+
+                // Bir sonraki rakamın ikinci olup olmadığını belirt
+                isSecond = !isSecond;
+            }
+
+            // Toplamın 10 ile tam bölünüp bölünmediğini kontrol et (mod 10)
+            return (sum % 10 == 0);
+        }
+    }
+}
---
+++ LuhnAlgorithmProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using LuhnAlgorithmProject;
+
+Console.WriteLine("Kart numarasını girin:");
+string cardNumber = Console.ReadLine();
+
+if (LuhnAlgorithm.IsValid(cardNumber))
+{
+    Console.WriteLine("Geçerli kart numarası.");
+}
+else
+{
+    Console.WriteLine("Geçersiz kart numarası.");
+}
